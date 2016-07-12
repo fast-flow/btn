4fe3d6a14329d79947828082d8b6f66ddd33c5eb
nimojs <nimo.jser@gmail.com>
2016-07-12 10:02:27 +0800

    build

--- fis-conf.js
+++ fis-conf.js
@@ -1,8 +1,24 @@
 var markrun = require('markrun')
-fis.match('*.md', {
+fis.match('README.md', {
     rExt: '.html',
+    release: 'demo.html',
     parser: function (content) {
-        var html = markrun(content)
+        var html = markrun(content, {
+            template: [
+            '<!DOCTYPE html>',
+            '<html lang="en">',
+            '<head>',
+                '<meta charset="UTF-8">',
+                '<meta name="viewport" content="width=device-width, initial-scale=1.0">',
+                '<meta http-equiv="X-UA-Compatible" content="ie=edge">',
+                '<title>Document</title>',
+            '</head>',
+            '<body>',
+            '<%- content %>',
+            '</body>',
+            '</html>'
+            ].join('\n')
+        })
         html = html.replace(/href="([^"]+)\.md"/g, 'href="$1.html"')
         return html
     }
@@ -11,4 +27,7 @@ fis.match('*.md', {
 fis.match('*.less', {
     rExt: '.css',
     parser: fis.plugin('less-2.x')
+})
+fis.match('package.json', {
+    release: false
 })
